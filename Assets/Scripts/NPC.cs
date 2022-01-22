9d5b69a96dbbb2b669d17104a18f93dadc9d5639
Eliot Lash <fadookie@gmail.com>
2022-01-22 15:14:59 -0800

    Make NPC AI idle path to random location anywhere on the playing field

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -16,11 +16,15 @@ public class NPC : MonoBehaviour
     [SerializeField] private Material whiteMaterial;
     [SerializeField] private MeshRenderer meshRenderer;
     [SerializeField] private float playerNoticeDistance = 10f;
+    
     private NavMeshAgent navMeshAgent;
+    [SerializeField] private float pathEndThreshold = 0.1f;
+    private bool hasPath = false;
 
     private bool dead;
 
     private Coroutine wanderRoutine;
+    private Collider groundCollider;
 
     public void Setup(GameColor gameColor)
     {
@@ -29,6 +33,10 @@ public class NPC : MonoBehaviour
 
     protected void Start() {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        if (Application.isPlaying) {
+            var ground = GameObject.FindGameObjectWithTag("Ground");
+            groundCollider = ground.GetComponent<MeshCollider>();
+        }
     }
 
     protected void Update()
@@ -39,58 +47,84 @@ public class NPC : MonoBehaviour
         }
         gameObject.layer = GetNPCLayer();
 
-        var playerPos = Player.Instance.ObservablePosition.Value;
-        var playerColor = Player.Instance.CurrentColor;
-        var distanceToPlayer = Vector3.Distance(playerPos, transform.position);
-        if (distanceToPlayer > playerNoticeDistance) {
-            // Wander around
-            if (wanderRoutine == null) {
-                wanderRoutine = StartCoroutine(Wander());
-            }
-        } else {
-            if (wanderRoutine != null) {
-                StopCoroutine(wanderRoutine);
-                wanderRoutine = null;
-            }
-                    if (!playerColor.Equals(color))
-                    {
-        //                Debug.Log(string.Format("NPC {0} run away from player {1}", color, playerColor));
-                // run away
-                var awayFromPlayer = (transform.position - playerPos).normalized * 20;
-                NavMeshHit closestHit;
-                if (NavMesh.SamplePosition(awayFromPlayer, out closestHit, 100f,
-                            navMeshAgent.areaMask))
-                        {
-                    SafeSetDestination(closestHit.position);
+        if (Application.isPlaying) {
+            var playerPos = Player.Instance.ObservablePosition.Value;
+            var playerColor = Player.Instance.CurrentColor;
+            var distanceToPlayer = Vector3.Distance(playerPos, transform.position);
+            if (distanceToPlayer > playerNoticeDistance) {
+                // Wander around
+                if (AtEndOfPath()) {
+                    var bounds = groundCollider.bounds;
+                    var newTarget = new Vector3(UnityEngine.Random.Range(bounds.min.x, bounds.max.x), 0,
+                        UnityEngine.Random.Range(bounds.min.z, bounds.max.z));
+                    Debug.Log(string.Format("wander new target newTarget:{0} pathStatus:{1} pathPending:{2}", newTarget,
+                        navMeshAgent.pathStatus, navMeshAgent.pathPending));
+                    SafeSetDestination(newTarget);
                 }
+            } else {
+                if (wanderRoutine != null) {
+                    Debug.LogWarning("Wander stop");
+                    StopCoroutine(wanderRoutine);
+                    wanderRoutine = null;
+                }
+                if (!playerColor.Equals(color)) {
+                    //                Debug.Log(string.Format("NPC {0} run away from player {1}", color, playerColor));
+                    // run away
+                    var awayFromPlayer = (transform.position - playerPos).normalized * 20;
+                    NavMeshHit closestHit;
+                    if (NavMesh.SamplePosition(awayFromPlayer, out closestHit, 100f,
+                        navMeshAgent.areaMask)) {
+                        SafeSetDestination(closestHit.position);
                     }
-                    else
-                    {
-        //                Debug.Log(string.Format("NPC {0} chase player {1}", color, playerColor));
-                // chase player
-                SafeSetDestination(playerPos);
+                } else {
+                    //                Debug.Log(string.Format("NPC {0} chase player {1}", color, playerColor));
+                    // chase player
+                    SafeSetDestination(playerPos);
+                }
             }
         }
     }
 
     private void SafeSetDestination(Vector3 pos) {
         if (navMeshAgent.isActiveAndEnabled && navMeshAgent.isOnNavMesh) {
-            navMeshAgent.destination = pos;
+            if (!navMeshAgent.SetDestination(pos)) {
+                Debug.LogError("Unable to set destination");
+            }
         } 
     }
 
-    private IEnumerator Wander() {
-        while(true) {
-            const float randomDistance = 3f;
-            var newTarget = transform.position + new Vector3(UnityEngine.Random.Range(-randomDistance, randomDistance), 0, UnityEngine.Random.Range(-randomDistance, randomDistance));
-            NavMeshHit closestHit;
-            if (NavMesh.SamplePosition(newTarget, out closestHit, 100f,
-                navMeshAgent.areaMask)) {
-                SafeSetDestination(closestHit.position);
-            }
-            yield return new WaitForSeconds(0.75f);
+//    private IEnumerator Wander() {
+//        Debug.Log("Wander routine start");
+//        while(true) {
+////            yield return new WaitForSeconds(1f);
+////            NavMeshHit closestHit;
+////            if (NavMesh.SamplePosition(newTarget, out closestHit, 100f,
+////                navMeshAgent.areaMask)) {
+////                Debug.Log(string.Format("wander SetDestination:{0}", closestHit.position));
+////                SafeSetDestination(closestHit.position);
+////            } else {
+////                Debug.LogWarning("wander no closest point, Bail");
+////            }
+//            while(!AtEndOfPath()) {
+//                yield return null;
+//            }
+//            Debug.LogWarning("wander path complete");
+//        }
+//    }
+    
+    bool AtEndOfPath()
+    {
+        hasPath |= navMeshAgent.hasPath;
+        if (hasPath && navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance + pathEndThreshold )
+        {
+            // Arrived
+            hasPath = false;
+            return true;
         }
+ 
+        return false;
     }
+    
 
     private Material GetMaterial()
     {
